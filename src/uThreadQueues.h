baa1899add522275779d140303513881cde5d3b6
Henry Qin <hq6@stanford.edu>
2018-08-15 14:07:10 -0700

    Add sanity checks for item value

--- src/uThreadQueues.h
+++ src/uThreadQueues.h
@@ -4,19 +4,29 @@
 #include "AbstractMPSCQ.h"
 #include "MPSCQueue.h"
 
-class Dummy: public MPSCQueue<Dummy>::Node { };
+class Dummy: public MPSCQueue<Dummy>::Node {
+  public:
+    int value;
+};
 /**
   * Elements here must inherit from Node.
   */
 class IntrusiveNonBlockingQueue : public AbstractMPSCQ {
   public:
     virtual bool enqueue(void* item) {
-        internalQueue.push(*reinterpret_cast<Dummy*>(item));
+        Dummy* dummy = reinterpret_cast<Dummy*>(item);
+        dummy->value = 7;
+        internalQueue.push(*dummy);
         return true;
     }
 
     virtual bool dequeue(void** item) {
         *item = internalQueue.pop();
+        Dummy* dummy = reinterpret_cast<Dummy*>(*item);
+        if (dummy != nullptr && dummy->value != 7) {
+            fprintf(stderr, "Data was not preserved! Exiting...");
+            abort();
+        }
         return *item != nullptr;
     }
 
